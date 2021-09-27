5eb9e5d8edd44886daa97e4fe548b5b674686db0
Matt Donnelly <mattdonnelly@me.com>
2021-09-27 20:31:52 +0100

    Update nvim-tree

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,18 +1,24 @@
 local keymap = vim.api.nvim_set_keymap
 keymap("n", "<leader>n", ":NvimTreeToggle<CR>", {})
 
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-vim.g.nvim_tree_width = 40
 vim.g.nvim_tree_ignore = { '.git', '.cache' }
-vim.g.nvim_tree_auto_open = 1
-vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' }
 vim.g.nvim_tree_quit_on_open = 1
-vim.g.nvim_tree_follow = 1
-vim.g.nvim_tree_disable_netrw = 1
 vim.g.nvim_tree_hide_dotfiles = 0
-vim.g.nvim_tree_allow_resize = 1
 vim.g.nvim_tree_icons = {
-  default = '',
+  default = '',
   symlink = '',
+}
+require('nvim-tree').setup {
+  open_on_setup       = false,
+  disable_netrw       = true,
+  auto_close          = true,
+  lsp_diagnostics     = true,
+  ignore_ft_on_setup  = { 'startify', 'dashboard' },
+  update_focused_file = {
+    enable = true,
+  },
+  view = {
+    width       = 40,
+    auto_resize = true
+  },
 }
