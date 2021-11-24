047862339c2821f606570a9bbdcc48ac73969c74
Matt Donnelly <matthew.donnelly@intercom.io>
2021-11-24 22:39:19 +0000

    Lots of LSP fixes

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -1,21 +1,22 @@
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
+local null_ls = require("null-ls")
 local ts_utils = require("nvim-lsp-ts-utils")
 
-vim.g.coq_settings = { auto_start = 'shut-up' }
-local coq = require('coq')
+vim.g.coq_settings = { auto_start = "shut-up" }
+local coq = require("coq")
 
-vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
-vim.lsp.handlers['textDocument/references'] = require'lsputil.locations'.references_handler
-vim.lsp.handlers['textDocument/definition'] = require'lsputil.locations'.definition_handler
-vim.lsp.handlers['textDocument/declaration'] = require'lsputil.locations'.declaration_handler
-vim.lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
-vim.lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
-vim.lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
-vim.lsp.handlers['workspace/symbol'] = require'lsputil.symbols'.workspace_handler
+vim.lsp.handlers["textDocument/codeAction"] = require"lsputil.codeAction".code_action_handler
+vim.lsp.handlers["textDocument/references"] = require"lsputil.locations".references_handler
+vim.lsp.handlers["textDocument/definition"] = require"lsputil.locations".definition_handler
+vim.lsp.handlers["textDocument/declaration"] = require"lsputil.locations".declaration_handler
+vim.lsp.handlers["textDocument/typeDefinition"] = require"lsputil.locations".typeDefinition_handler
+vim.lsp.handlers["textDocument/implementation"] = require"lsputil.locations".implementation_handler
+vim.lsp.handlers["textDocument/documentSymbol"] = require"lsputil.symbols".document_handler
+vim.lsp.handlers["workspace/symbol"] = require"lsputil.symbols".workspace_handler
 
-local signs = { Error = ' ', Warning = ' ', Hint = ' ', Information = ' ' }
+local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 for type, icon in pairs(signs) do
-  local hl_name = vim.fn.has('nvim-0.6') and "LspDiagnosticsSign" or "DiagnosticSign"
+  local hl_name = vim.fn.has("nvim-0.6") and "DiagnosticSign" or "LspDiagnosticsSign"
   local hl = hl_name .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
@@ -27,35 +28,35 @@ local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
   -- Mappings.
   local opts = { noremap=true, silent=true }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '<space>=', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+  buf_set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
   if client.resolved_capabilities.document_formatting then
     vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
   end
 end
 
--- Use a loop to conveniently call 'setup' on multiple servers and
+-- Use a loop to conveniently call "setup" on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local default_config = {
   on_attach = on_attach,
@@ -75,7 +76,12 @@ lspconfig.cssls.setup(coq.lsp_ensure_capabilities(default_config))
 lspconfig.bashls.setup(coq.lsp_ensure_capabilities(default_config))
 lspconfig.jsonls.setup(coq.lsp_ensure_capabilities(default_config))
 lspconfig.vimls.setup(coq.lsp_ensure_capabilities(default_config))
-lspconfig.flow.setup(coq.lsp_ensure_capabilities(default_config))
+lspconfig.flow.setup(coq.lsp_ensure_capabilities({
+  on_attach = on_attach,
+  flags = {
+    debounce_text_changes = 150,
+  }
+}))
 lspconfig.tsserver.setup(coq.lsp_ensure_capabilities({
   on_attach = function(client, bufnr)
     client.resolved_capabilities.document_formatting = false
@@ -101,10 +107,19 @@ lspconfig.tsserver.setup(coq.lsp_ensure_capabilities({
     debounce_text_changes = 150,
   },
   root_dir = function(fname)
-    return not lspconfig.util.root_pattern('.flowconfig')(fname) 
-    and lspconfig.util.root_pattern('package.json', 'jsconfig.json', '.git')(fname)
+    return lspconfig.util.root_pattern("tsconfig.json")(fname)
+    and not lspconfig.util.root_pattern(".flowconfig")(fname) 
+    and lspconfig.util.root_pattern("package.json", "jsconfig.json", ".git")(fname)
   end
 }))
 
-require("null-ls").config({})
+local sources = {
+  null_ls.builtins.formatting.prettier,
+  null_ls.builtins.diagnostics.eslint_d,
+  null_ls.builtins.code_actions.eslint_d,
+}
+
+null_ls.config({ 
+  sources = sources,
+})
 lspconfig["null-ls"].setup({ on_attach = on_attach })
