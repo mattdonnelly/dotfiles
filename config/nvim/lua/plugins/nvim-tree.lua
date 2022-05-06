afecbf8a2d4e39480c4ed1fd5378ed0705072786
Matt Donnelly <mattdonnelly@Matts-MacBook-Pro.local>
2022-05-07 00:01:01 +0100

    Update to nvim-tree to latest API

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,16 +1,19 @@
 local keymap = vim.api.nvim_set_keymap
 keymap("n", "<leader>n", ":NvimTreeToggle<CR>", {})
 
-vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_icons = {
   default = '',
   symlink = '',
 }
 require('nvim-tree').setup {
-  open_on_setup       = false,
   disable_netrw       = true,
-  auto_close          = true,
   ignore_ft_on_setup  = { 'startify', 'dashboard' },
+  actions = {
+    open_file = {
+      quit_on_open  = true,
+      resize_window = true
+    }
+  },
   diagnostics = {
     enable = true,
   },
@@ -22,7 +25,6 @@ require('nvim-tree').setup {
     custom = { '.git', '.cache' }
   },
   view = {
-    width       = 40,
-    auto_resize = true
+    width = 40,
   },
 }
