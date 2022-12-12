262157162352123c84b5d7a5f0ec531c3a4034b9
Matt Donnelly <matthew.donnelly@intercom.io>
2022-12-12 12:08:33 +0000

    Fix formatting for js + ruby

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -1,7 +1,6 @@
 local lspconfig = require("lspconfig")
 local null_ls = require("null-ls")
 local null_ls_helpers = require("null-ls.helpers")
-local ts_utils = require("nvim-lsp-ts-utils")
 
 vim.g.coq_settings = {
   auto_start = "shut-up",
@@ -29,12 +28,25 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name ~= "tsserver" and client.name ~= "solargraph" and client.name ~= "ember"
+    end,
+    bufnr = bufnr,
+  })
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   --Enable completion triggered by <c-x><c-o>
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- vim.api.nvim_command('autocmd CursorHold <buffer> lua vim.diagnostic.open_float(0,{scope=\'line\'})')
 
   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
@@ -53,19 +65,21 @@ local on_attach = function(client, bufnr)
   buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
   buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+  buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float(0,{scope=\'line\'})<CR>", opts)
   buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
   buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
   buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-  buf_set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  buf_set_keymap("n", "<space>=", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
 
-  if client.resolved_capabilities.document_formatting then
-    vim.cmd([[
-      augroup LspFormatting
-      autocmd! * <buffer>
-      autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 2000)
-      augroup END
-    ]])
+  if client.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        lsp_formatting(bufnr)
+      end,
+    })
   end
 end
 
@@ -80,8 +94,8 @@ local default_config = {
 
 lspconfig.ember.setup(coq.lsp_ensure_capabilities({
   on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
 
     on_attach(client, bufnr)
   end,
@@ -89,8 +103,6 @@ lspconfig.ember.setup(coq.lsp_ensure_capabilities({
 }))
 lspconfig.solargraph.setup(coq.lsp_ensure_capabilities({
   on_attach = function(client, bufnr) 
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
     on_attach(client, bufnr)
   end,
   flags = {
@@ -110,34 +122,23 @@ lspconfig.cssls.setup(coq.lsp_ensure_capabilities(default_config))
 lspconfig.bashls.setup(coq.lsp_ensure_capabilities(default_config))
 -- lspconfig.jsonls.setup(coq.lsp_ensure_capabilities(default_config))
 lspconfig.vimls.setup(coq.lsp_ensure_capabilities(default_config))
-lspconfig.tsserver.setup(coq.lsp_ensure_capabilities({
-  on_attach = function(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
-    client.resolved_capabilities.document_range_formatting = false
-
-    ts_utils.setup({
-      import_all_timeout = 5000,
-      import_all_priorities = {
-        same_file = 1,
-        local_files = 2,
-        buffer_content = 3,
-        buffers = 4,
-      },
-      import_all_scan_buffers = 100,
-      import_all_select_source = false,
-    })
-    ts_utils.setup_client(client)
-
-    local opts = { noremap=true, silent=true }
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspRenameFile<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "go", ":TSLspImportAll<CR>", opts)
-
-    on_attach(client, bufnr)
-  end,
-  flags = {
-    debounce_text_changes = 150,
+require('typescript').setup(coq.lsp_ensure_capabilities({
+  go_to_source_definition = {
+    fallback = true, -- fall back to standard LSP definition on failure
   },
+  server = {
+    on_attach = function(client, bufnr)
+      local opts = { noremap=true, silent=true }
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspRenameFile<CR>", opts)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "go", ":TSLspImportAll<CR>", opts)
+
+      on_attach(client, bufnr)
+    end,
+    flags = {
+      debounce_text_changes = 150,
+    },
+  }
 }))
 
 local ember_template_lint = {
@@ -150,32 +151,25 @@ local ember_template_lint = {
   }
 }
 
+local command_resolver = require("null-ls.helpers.command_resolver")
 local sources = {
   ember_template_lint,
   null_ls.builtins.formatting.prettier.with({
-    disabled_filetypes = { 'html.handlebars' },
-    dynamic_command = require("null-ls.helpers.command_resolver").from_node_modules
+    disabled_filetypes = { 'html.handlebars', 'json' },
+    dynamic_command = command_resolver.from_node_modules()
   }),
   null_ls.builtins.diagnostics.eslint.with({
-    dynamic_command = require("null-ls.helpers.command_resolver").from_node_modules
+    dynamic_command = command_resolver.from_node_modules()
   }),
   null_ls.builtins.code_actions.eslint.with({
-    dynamic_command = require("null-ls.helpers.command_resolver").from_node_modules
+    dynamic_command = command_resolver.from_node_modules()
   }),
   null_ls.builtins.formatting.rubocop,
   null_ls.builtins.diagnostics.rubocop,
 }
 
 null_ls.setup({
+  debug = true,
   sources = sources,
-  on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd([[
-      augroup NullLsFormatting
-      autocmd! * <buffer>
-      autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 2000)
-      augroup END
-      ]])
-    end
-  end,
+  on_attach = on_attach,
 })
