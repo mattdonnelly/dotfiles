701d568c839992382dee314de30462916e25a374
Matt Donnelly <matthew.donnelly@intercom.io>
2022-12-27 16:41:50 +0000

    Switch from lightspeed to leap

---
+++ config/nvim/lua/config/plugins/leap.lua
@@ -0,0 +1,12 @@
+return {
+  'ggandor/leap.nvim',
+  event = 'VeryLazy',
+
+  dependencies = {
+    { 'ggandor/flit.nvim', config = { labeled_modes = 'nv' } },
+  },
+
+  config = function()
+    require('leap').add_default_mappings()
+  end,
+}
--- config/nvim/lua/config/plugins/lightspeed.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'ggandor/lightspeed.nvim',
-  config = function()
-    require('lightspeed').setup({
-      match_only_the_start_of_same_char_seqs = true,
-      limit_ft_matches = 5,
-    })
-  end
-}
