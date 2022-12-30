758221724dd24bb79b895e1b088d94c5ca51ddfc
Matt Donnelly <matthew.donnelly@intercom.io>
2022-12-30 01:45:38 +0000

    Tweak diagnostics window

--- config/nvim/lua/user/plugins/lsp/diagnostics.lua
+++ config/nvim/lua/user/plugins/lsp/diagnostics.lua
@@ -5,10 +5,19 @@ M.signs = { Error = " ", Warning = " ", Hint = " ", Information = " 
 
 function M.setup()
   vim.diagnostic.config({
+    signs = true,
     underline = true,
     update_in_insert = false,
-    virtual_text = { spacing = 4, prefix = "●" },
+    virtual_text = { spacing = 2, prefix = "●" },
     severity_sort = true,
+    float = {
+      focusable = false,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
   })
   for type, icon in pairs(M.signs) do
     local hl_name = vim.fn.has("nvim-0.6") and "DiagnosticSign" or "LspDiagnosticsSign"
