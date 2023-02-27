54689705039a46a32a3562a2e8219483a20a2158
Matt Donnelly <matthew.donnelly@intercom.io>
2023-02-27 11:08:39 +0000

    Update dashboard config

--- config/nvim/lua/user/plugins/dashboard.lua
+++ config/nvim/lua/user/plugins/dashboard.lua
@@ -1,37 +1,39 @@
 return {
   "glepnir/dashboard-nvim",
-  event = "BufWinEnter",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    local db = require("dashboard")
-
-    db.custom_header = {
-      "                                        ",
-      " g@@@@@@@@@@@@@@@@@@@@@@@@b_            ",
-      "0@@@@@@@@@@@@@@@@@@@@@@@@@@@k           ",
-      "0@@@@@@@@@@@@@^^#@@@@@@@@@@@@L          ",
-      " #@@@@@@@@@@'   J@@@@@@@@@@@@@          ",
-      "               J@@@@@@@@@@@@@@b         ",
-      "              d@@@@@##@@@@@@@@@L        ",
-      "             d@@@@#   ^@@@@@@@@Q        ",
-      "            d@@@@@@r    #@@@@@@@[       ",
-      "           d@@@@@@@[     #@@@@@@@r      ",
-      "          0@@@@@@@P       0@@@@@@%      ",
-      "         0@@@@P            0@@@@@@L     ",
-      "        0@@@@^              0@@@@@@     ",
-      "       #@@@F                 0@@@@@b    ",
-      "      1@@@^                   `@@@@@L   ",
-      "                               ^@@@@@   ",
-      "                                ^@@@@[  ",
-      "                                  ##P   ",
-      "                                        ",
-    }
-
-    db.custom_center = {
-      { icon = "  ", desc = "Find file                   ", shortcut = "SPC f f", action = "Telescope find_files" },
-      { icon = "  ", desc = "Recent files                ", shortcut = "SPC f o", action = "Telescope oldfiles" },
-      { icon = "  ", desc = "Find word                   ", shortcut = "SPC f /", action = "Telescope live_grep" },
-      { icon = "  ", desc = "New file                    ", shortcut = "SPC f n", action = "enew" },
-      { icon = "  ", desc = "Settings                 ", shortcut = "e $MYVIMRC", action = "edit $MYVIMRC" },
-    }
+    require("dashboard").setup({
+      theme = "hyper",
+      config = {
+        shortcut = {
+          { desc = " Update", group = "@property", action = "Lazy update", key = "u" },
+          {
+            desc = " File",
+            group = "Label",
+            action = "Telescope find_files",
+            key = "f",
+          },
+          {
+            desc = " Search",
+            group = "DiagnosticHint",
+            action = "Telescope live_grep",
+            key = "/",
+          },
+          {
+            desc = " New",
+            group = "String",
+            action = "enew",
+            key = "n",
+          },
+          {
+            desc = " Settings",
+            group = "Number",
+            action = "edit $MYVIMRC",
+            key = "e",
+          },
+        },
+      },
+    })
   end,
 }
