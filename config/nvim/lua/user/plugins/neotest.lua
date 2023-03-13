0a3fc6898c26c8bfab3b5e3288336b181327aaee
Matt Donnelly <matthew.donnelly@intercom.io>
2023-03-13 22:03:16 +0000

    Update vscode dap

--- config/nvim/lua/user/plugins/neotest.lua
+++ config/nvim/lua/user/plugins/neotest.lua
@@ -11,7 +11,7 @@ return {
     require("neotest").setup({
       adapters = {
         require("neotest-jest")({
-          jestCommand = vim.g.jest_command or "npm test --",
+          jestCommand = vim.g.jest_command or "npm test",
         }),
         require("neotest-rspec")({
           rspecCommand = vim.g.rspec_command or "bundle exec rspec",
