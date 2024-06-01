0b1c1b21f11dd3ae8333a502693d5f28556994fa
Matt Donnelly <services@mattdonnelly.me>
2024-06-01 21:38:59 +0100

    Add mini.move

--- config/nvim/lua/user/plugins/mini.lua
+++ config/nvim/lua/user/plugins/mini.lua
@@ -27,6 +27,23 @@ return {
     end,
   },
   {
+    "echasnovski/mini.move",
+    version = false,
+    event = "VeryLazy",
+    opts = {
+      mappings = {
+        left = "<C-h>",
+        right = "<C-l>",
+        down = "<C-j>",
+        up = "<C-k>",
+        line_left = "<C-h>",
+        line_down = "<C-j>",
+        line_up = "<C-k>",
+        line_right = "<C-l>",
+      },
+    },
+  },
+  {
     "echasnovski/mini.pairs",
     version = false,
     event = "VeryLazy",
