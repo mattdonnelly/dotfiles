c319b8ba83879f1617143edc811a4227cb4ed838
Matt Donnelly <services@mattdonnelly.me>
2024-06-01 17:17:43 +0100

    Update nvim-notify config

--- config/nvim/lua/user/plugins/noice.lua
+++ config/nvim/lua/user/plugins/noice.lua
@@ -24,11 +24,23 @@ return {
     },
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    {
+      "rcarriga/nvim-notify",
+      opts = {
+        stages = "fade",
+        render = "compact",
+        timeout = 500,
+        max_height = function()
+          return math.floor(vim.o.lines * 0.75)
+        end,
+        max_width = function()
+          return math.floor(vim.o.columns * 0.60)
+        end,
+        on_open = function(win)
+          vim.api.nvim_win_set_config(win, { zindex = 100 })
+        end,
+      },
+    },
   },
 }
