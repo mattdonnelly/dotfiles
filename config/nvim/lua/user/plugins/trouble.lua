57b7163922338943564c57c021f47e49fa0433e1
Matthew Donnelly <services@mattdonnelly.me>
2024-06-03 20:26:29 +0100

    Fix trouble keymaps

--- config/nvim/lua/user/plugins/trouble.lua
+++ config/nvim/lua/user/plugins/trouble.lua
@@ -2,11 +2,20 @@ return {
   "folke/trouble.nvim",
   cmd = { "Trouble", "TroubleClose", "TroubleRefresh", "TroubleToggle" },
   keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", "Trouble diagnostics" },
-    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", "Trouble current buffer" },
-    { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", "Trouble loclist" },
-    { "<leader>xq", "<cmd>Trouble quickfix toggle<cr>", "Trouble quickfix" },
-    { "<leader>xp", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", "Trouble lsp references" },
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Trouble diagnostics" },
+    { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Trouble current buffer" },
+    { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Trouble loclist" },
+    { "<leader>xq", "<cmd>Trouble quickfix toggle<cr>", desc = "Trouble quickfix" },
+    {
+      "<leader>xs",
+      "<cmd>Trouble symbols toggle focus=false win.size=0.25<cr>",
+      desc = "Trouble symbols",
+    },
+    {
+      "<leader>xc",
+      "<cmd>Trouble lsp toggle focus=false win.position=right win.size=0.25<cr>",
+      desc = "Trouble LSP references/definitions/...",
+    },
   },
   config = true,
 }
