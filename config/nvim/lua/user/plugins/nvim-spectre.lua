12926dcb8a3870c466d738b9b6e1a5571a842015
Matthew Donnelly <services@mattdonnelly.me>
2024-06-03 11:52:20 +0100

    Move Spectre mappings

--- config/nvim/lua/user/plugins/nvim-spectre.lua
+++ config/nvim/lua/user/plugins/nvim-spectre.lua
@@ -1,7 +1,16 @@
 return {
   "nvim-pack/nvim-spectre",
-  cmd = { "Spectre" },
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  cmd = { "Spectre" },
+  keys = function()
+    -- stylua: ignore
+    return {
+      { "<leader>SS", function() require("spectre").toggle() end, desc = "Toggle Spectre" },
+      { "<leader>Sw", function() require("spectre").open_visual({ select_word = true }) end, desc = "Search the current word" },
+      { "<leader>Sv", function() require("spectre").open_visual() end, desc = "Search the visual selection" },
+      { "<leader>Sf", function() require("spectre").open_file_search() end, desc = "Search in file" },
+    }
+  end,
 }
