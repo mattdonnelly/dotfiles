d42994e423e661416068c0a4100df18f89d88657
Matt Donnelly <services@mattdonnelly.me>
2024-07-08 12:31:12 +0100

    Handle ESLint flat config

--- config/nvim/lua/user/plugins/lint.lua
+++ config/nvim/lua/user/plugins/lint.lua
@@ -10,5 +10,22 @@ return {
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
     }
+
+    local cwd = vim.loop.cwd()
+    local maybe_config_path = vim.fn.fnamemodify(cwd .. "/eslint.config.js", ":p")
+    local has_flat_config = vim.fn.filereadable(maybe_config_path) == 1
+
+    if has_flat_config then
+      local eslint_d = lint.linters.eslint_d
+      eslint_d.env = {
+        ["ESLINT_USE_FLAT_CONFIG"] = "true",
+        ["CORE_D_TITLE"] = "eslint_d_flat",
+        ["CORE_D_DOTFILE"] = ".eslint_d_flat",
+        ["PWD"] = cwd,
+        ["SHELL"] = os.getenv("SHELL"),
+        ["TMPDIR"] = os.getenv("TMPDIR"),
+        ["HOME"] = os.getenv("HOME"),
+      }
+    end
   end,
 }
