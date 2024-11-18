e8090fac56ff4f08ccc2be4910029ff4fc207734
Matt Donnelly <services@mattdonnelly.me>
2024-11-18 22:07:56 +0000

    Make lua_ls vim library aware

--- config/nvim/lua/user/plugins/lsp/init.lua
+++ config/nvim/lua/user/plugins/lsp/init.lua
@@ -71,6 +71,7 @@ return {
                   globals = { "vim" },
                 },
                 workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
                   checkThirdParty = false,
                 },
                 completion = {
